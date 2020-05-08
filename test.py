0e15906d06eec4005f62e16f057d7ffdeac08279
Changxing97 <1278729001@qq.com>
2020-05-08 13:47:22 +0800

    ChangXing 2020.05.08 - 2
    增加：第7天详细教程(Part1)

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-a = "1324"
-b = [1, 3, 2]
-c = (1, 3, 2, 5, 4)
-d = {1, 6, 5, 4, 2, 3}
-e = {"b": 5, "a": 1}
-print(sorted(a))
-print(sorted(b))
-print(sorted(c))
-print(sorted(d, reverse=True))
-print(sorted(e))
+print(list(range(5)))
+print(list(range(1, 5)))
+print(list(range(1, 5, 2)))
