0a81f034d695aa5b79133725c1802dcb46dcc0f7
ChangxingJiang <1278729001@qq.com>
2020-05-29 09:33:44 +0800

    更新代码：案例0402(猫眼网播热度采集)

--- Week-04/Example-0402/猫眼网播热度采集.py
+++ Week-04/Example-0402/猫眼网播热度采集.py
@@ -1,8 +1,11 @@
+import time
+
 from selenium import webdriver
 
 if __name__ == "__main__":
-    browser = webdriver.Chrome(executable_path=r"D:\Python38_64\chromedriver_81_0_4044_69.exe")  # ChromeDriver可执行文件的路径
+    browser = webdriver.Chrome(executable_path=r"D:\Python38_64\chromedriver_83_0_4103_39.exe")  # ChromeDriver可执行文件的路径
     browser.get("http://piaofang.maoyan.com/dashboard/web-heat")
+    time.sleep(1)
     for movie_label in browser.find_elements_by_css_selector(
             "#app > div > div > div.dashboard-content > div.dashboard-list.dashboard-left.bg > div.movielist-container > div > table > tbody > tr"):
         print("排名:", movie_label.find_element_by_class_name("moviename-index").text)
