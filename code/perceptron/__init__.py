dd2cdaeb316c4c256976cd3ef5192f360f6c8cb4
ChangxingJiang <1278729001@qq.com>
2021-06-10 21:57:27 +0800

    更新：《统计学习方法》啃书辅助 第2章

--- code/perceptron/__init__.py
+++ code/perceptron/__init__.py
@@ -1,3 +1,7 @@
 from ._dual_form import dual_form_perceptron
 from ._gram import count_gram
 from ._original_form import original_form_of_perceptron
+
+__all__ = ["count_gram",
+           "original_form_of_perceptron",
+           "dual_form_perceptron"]
